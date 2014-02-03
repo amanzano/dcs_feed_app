f6b24ceaf2aca531a6818d7b0fbecd3659d1d757
Audrey Manzano <amanzano@stratpoint.com>
2014-02-03 16:03:46 +0800

    Initial Commit

---
+++ MyApplication/ACMAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ACMAppDelegate.h
+//  MyApplication
+//
+//  Created by Audrey Manzano on 2/3/14.
+//  Copyright (c) 2014 Audrey Manzano. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ACMAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MyApplication/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MyApplication
+//
+//  Created by Audrey Manzano on 2/3/14.
+//  Copyright (c) 2014 Audrey Manzano. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "ACMAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([ACMAppDelegate class]));
+    }
+}
